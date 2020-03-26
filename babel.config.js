10b3e60a5023700ac75b8c78033ef504c4547fab
Jeff Barczewski <jeff.barczewski@gmail.com>
2020-03-25 22:56:02 -0500

    update dependencies that bumped major versions
    specify corejs to match core-js that is installed.

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,18 @@
 const BABEL_ENV = process.env.BABEL_ENV;
-const CI = (process.env.CI && process.env.CI === 'true');
+const CI = process.env.CI && process.env.CI === 'true';
 
 const presets = [
-  ['@babel/env', {
-    // targets are specified in .browserslist
-    // run `npx browserslist` will show resultant targets or see debug output from build
-    useBuiltIns: 'usage',
-    modules: (BABEL_ENV === 'es') ? false : 'auto', // not transforming modules for es
-    debug: (CI)  // show the browser target and plugins used when in CI mode
-  }]
+  [
+    '@babel/env',
+    {
+      // targets are specified in .browserslist
+      // run `npx browserslist` will show resultant targets or see debug output from build
+      useBuiltIns: 'usage',
+      corejs: '3.6.4',
+      modules: BABEL_ENV === 'es' ? false : 'auto', // not transforming modules for es
+      debug: CI // show the browser target and plugins used when in CI mode
+    }
+  ]
 ];
 
 const plugins = [];
@@ -16,14 +20,12 @@ const plugins = [];
 // these are merged with others
 const env = {
   cov: {
-    plugins: [
-      'istanbul'
-    ]
+    plugins: ['istanbul']
   }
 };
 
 module.exports = {
   env,
   plugins,
-  presets,
+  presets
 };
