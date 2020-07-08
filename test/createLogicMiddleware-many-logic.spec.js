d7cded445328cb0500210ac83d7fa5f227c8ddb3
Jeff Barczewski <jeff.barczewski@gmail.com>
2020-07-08 12:44:50 -0500

    update dependencies

--- test/createLogicMiddleware-many-logic.spec.js
+++ test/createLogicMiddleware-many-logic.spec.js
@@ -10,34 +10,36 @@ describe('createLogicMiddleware-many-logic', () => {
     let store;
 
     beforeEach(() => {
-      const arrLogic = range(0, NUM_LOGICS).map(() => createLogic({
-        type: 'foo',
-        validate({ action }, allow) {
-          allow({
-            ...action,
-            validates: action.validates + 1
-          });
-        },
-        process({ action }, dispatch, done) {
-          dispatch({ type: 'foo-success' });
-          done();
-        }
-      }));
+      const arrLogic = range(0, NUM_LOGICS).map(() =>
+        createLogic({
+          type: 'foo',
+          validate({ action }, allow) {
+            allow({
+              ...action,
+              validates: action.validates + 1,
+            });
+          },
+          process({ action }, dispatch, done) {
+            dispatch({ type: 'foo-success' });
+            done();
+          },
+        })
+      );
       mw = createLogicMiddleware(arrLogic);
       const reducer = (state = { validates: 0, processes: 0 }, action) => {
         switch (action.type) {
-        case 'foo' :
-          return {
-            ...state,
-            validates: state.validates + action.validates
-          };
-        case 'foo-success' :
-          return {
-            ...state,
-            processes: state.processes + 1
-          };
-        default:
-          return state;
+          case 'foo':
+            return {
+              ...state,
+              validates: state.validates + action.validates,
+            };
+          case 'foo-success':
+            return {
+              ...state,
+              processes: state.processes + 1,
+            };
+          default:
+            return state;
         }
       };
       store = createStore(reducer, undefined, applyMiddleware(mw));
@@ -55,25 +57,27 @@ describe('createLogicMiddleware-many-logic', () => {
     let store;
 
     beforeEach(() => {
-      const arrLogic = range(0, NUM_LOGICS).map(() => createLogic({
-        type: 'foo',
-        validate({ action }, allow) {
-          allow({
-            ...action,
-            validates: action.validates + 1
-          });
-        }
-      }));
+      const arrLogic = range(0, NUM_LOGICS).map(() =>
+        createLogic({
+          type: 'foo',
+          validate({ action }, allow) {
+            allow({
+              ...action,
+              validates: action.validates + 1,
+            });
+          },
+        })
+      );
       mw = createLogicMiddleware(arrLogic);
       const reducer = (state = { validates: 0, processes: 0 }, action) => {
         switch (action.type) {
-        case 'foo' :
-          return {
-            ...state,
-            validates: state.validates + action.validates
-          };
-        default:
-          return state;
+          case 'foo':
+            return {
+              ...state,
+              validates: state.validates + action.validates,
+            };
+          default:
+            return state;
         }
       };
       store = createStore(reducer, undefined, applyMiddleware(mw));
@@ -88,43 +92,45 @@ describe('createLogicMiddleware-many-logic', () => {
   describe('with process', () => {
     // single-test 240, with mergeMapOrTap 450
     // full suite 350, with mergeMapOrTap 540
-    const NUM_LOGICS = 280; // 350 with optimizations
-    let mw;
+    const NUM_LOGICS = 600; // 350 with optimizations
+    let mw1, mw2;
     let store;
 
-    beforeEach(bDone => {
-      const arrLogic = range(0, NUM_LOGICS).map(() => createLogic({
-        type: 'foo',
-        process({ action }, dispatch, done) {
-          dispatch({ type: 'foo-success' });
-          done();
-        }
-      }));
-      mw = createLogicMiddleware(arrLogic);
+    beforeEach(async () => {
+      const arrLogic = range(0, NUM_LOGICS).map(() =>
+        createLogic({
+          type: 'foo',
+          process({ action }, dispatch, done) {
+            dispatch({ type: 'foo-success' });
+            done();
+          },
+        })
+      );
+      mw1 = createLogicMiddleware(arrLogic.slice(0, 300));
+      mw2 = createLogicMiddleware(arrLogic.slice(300));
       const reducer = (state = { validates: 0, processes: 0 }, action) => {
         switch (action.type) {
-        case 'foo' :
-          return {
-            ...state,
-            validates: state.validates + action.validates
-          };
-        case 'foo-success' :
-          return {
-            ...state,
-            processes: state.processes + 1
-          };
-        default:
-          return state;
+          case 'foo':
+            return {
+              ...state,
+              validates: state.validates + action.validates,
+            };
+          case 'foo-success':
+            return {
+              ...state,
+              processes: state.processes + 1,
+            };
+          default:
+            return state;
         }
       };
-      store = createStore(reducer, undefined, applyMiddleware(mw));
+      store = createStore(reducer, undefined, applyMiddleware(mw1, mw2));
       store.dispatch({ type: 'foo', validates: 0 });
-      mw.whenComplete(() => bDone());
+      return Promise.all([mw1.whenComplete(), mw2.whenComplete()]);
     });
 
     it('expect state to be updated', () => {
       expect(store.getState()).toEqual({ validates: 0, processes: NUM_LOGICS });
     });
   });
-
 });
